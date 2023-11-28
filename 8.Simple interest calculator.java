399c8ee1e7666d672b01d593b353c934d2209ee7
Bhavatharanig <112235144+Bhavatharanig@users.noreply.github.com>
2023-11-28 10:37:05 +0530

    Create 8.Simple interest calculator.java

---
+++ 8.Simple interest calculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+
+    // Function to calculate simple interest
+    static double calculateSimpleInterest(double principal, double rate, double time) {
+        // Senior citizen interest rate is 12%, others have 10% interest rate
+        double interestRate = (principal >= 100000) ? 0.12 : 0.10;
+
+        // Calculate simple interest
+        double simpleInterest = (principal * interestRate * time) / 100;
+
+        return simpleInterest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input principal amount, time, and check if the customer is a senior citizen
+        System.out.print("Enter the principal amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter the time (in years): ");
+        double time = scanner.nextDouble();
+
+        System.out.print("Is the customer a senior citizen? (true/false): ");
+        boolean isSeniorCitizen = scanner.nextBoolean();
+
+        // Calculate and display simple interest
+        double simpleInterest = calculateSimpleInterest(principal, 0, time);
+        System.out.println("Simple Interest: " + simpleInterest);
+
+        scanner.close();
+    }
+}
+
+OUTPUT:
+C:\Users\BHAVATHARANI\Desktop>javac SimpleInterestCalculator.java
+
+C:\Users\BHAVATHARANI\Desktop>java SimpleInterestCalculator
+Enter the principal amount: 20000
+Enter the time (in years): 2
+Is the customer a senior citizen? (true/false): true
+Simple Interest: 40.0
