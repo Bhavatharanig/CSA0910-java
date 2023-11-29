cf34e127dd371e2d23b5a27315fe27dd8b71249a
Bhavatharanig <112235144+Bhavatharanig@users.noreply.github.com>
2023-11-29 22:42:52 +0530

    Create 26.Last word length.java

---
+++ 26.Last word length.java
@@ -0,0 +1,24 @@
+public class LastWordLength {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        int result = lengthOfLastWord(s);
+        System.out.println("Output: " + result);
+    }
+
+    public static int lengthOfLastWord(String s) {
+        // Trim the input string to remove leading and trailing spaces
+        s = s.trim();
+
+        // Find the index of the last space in the trimmed string
+        int lastSpaceIndex = s.lastIndexOf(' ');
+
+        // Calculate the length of the last word based on the last space index
+        return s.length() - lastSpaceIndex - 1;
+    }
+}
+
+OUTPUT:
+C:\Users\BHAVATHARANI\Documents>javac LastWordLength.java
+
+C:\Users\BHAVATHARANI\Documents>java LastWordLength
+Output: 5
